46eee27356a86ff030fc61cb7a0a47e7d02cff81
Peter Paredes <peter.paredes.info@gmail.com>
2025-04-12 02:33:56 -0700

    otro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{
-      backgroundColor: 'DarkMidnightBlue',
+      backgroundColor: '#003366',
       color: 'white',
       minHeight: '100vh',
       padding: '2rem',

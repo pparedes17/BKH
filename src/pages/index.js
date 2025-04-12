8828440f6f3a1797ac5dd75806f1b1b57ff4f958
Peter Paredes <peter.paredes.info@gmail.com>
2025-04-12 10:16:07 -0700

    otro

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
       position: 'relative', 
       minHeight: '100vh', 
       margin: 0, 
-      backgroundColor: '#0F4D92', // '#0D1B2A', '#1B365D',
+      backgroundColor: '#B0B45', // '#0D1B2A', '#1B365D',
       color: 'white',
       overflow: 'hidden' 
     }}>

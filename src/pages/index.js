f3fc5f55d90ed302212fc1e4e5892f6ce04613bb
Peter Paredes <peter.paredes.info@gmail.com>
2025-04-12 01:17:27 -0700

    Initial upload with logo and homepage

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <img src="/logo.png" alt="BridgeKey Homes Logo" style={{ maxWidth: '200px', marginBottom: '1rem' }} />
+      <h1>Welcome to BridgeKey Homes</h1>
+      <p>Your key to smart home ownership solutions.</p>
+    </div>
+  );
+}
